4fe1f4a68123fe3074e3bfec35943ad5367e780f
Soyeon <83504325+iamsoyeon@users.noreply.github.com>
2022-10-02 15:48:42 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -6,7 +6,7 @@ int main()
 {
 	int n[10];
 	int arr[10]; 
-	int unique = 0;
+	int rp = 0;
 	for(int i = 0; i < 10; i++)
 	{
 		cin >> n[i];
@@ -18,8 +18,8 @@ int main()
 	for(int i =0; i<9; i++)
 	{
 		if(arr[i] == arr[i+1])
-		unique += 1;
+		rp += 1;
 
 	}
-	cout << 10-unique;
+	cout << 10-rp;
 }
