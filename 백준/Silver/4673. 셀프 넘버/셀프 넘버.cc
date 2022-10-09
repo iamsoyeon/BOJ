f3b493787cae42350e42db26110fc67ff3896e66
Soyeon <83504325+iamsoyeon@users.noreply.github.com>
2022-10-09 10:02:08 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+ 
+using namespace std;
+ 
+int self(int number) {
+	int sum = number;
+ 
+	while (number != 0) {
+		sum = sum + (number % 10); // 첫 째 자리수ˇ
+		number = number / 10;	   // 10을 나누어 첫 째 자리를 없앤다.
+	}
+	return sum;
+}
+ 
+ 
+int main() {
+    
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<bool> selfOrNot(10000, false);
+    
+	for (int i = 1; i < 10001; i++) {
+		int n = self(i);
+		if (n < 10001) { // 10000 이 넘는 수는 필요가 없다.
+			selfOrNot[n] = true;
+		}
+	}
+	for (int i = 1; i < 10001; i++) {
+		if (!selfOrNot[i]) {
+			cout << i << "\n";
+		}
+	}
+ 
+	return 0;
+}
