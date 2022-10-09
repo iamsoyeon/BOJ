ad4c1a67988376d860307973a710a48bd55c61f8
Soyeon <83504325+iamsoyeon@users.noreply.github.com>
2022-10-09 10:30:15 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    char a;
+    int n;
+    
+    cin >> a;
+    n = (int)a;
+    
+    cout << n;
+    
+}
